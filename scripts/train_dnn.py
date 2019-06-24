1ad5958dced1ea297857e96859eeba020d4e7dc8
Laszlo Dobos <dobos@complex.elte.hu>
2019-06-24 19:06:52 -0400

    Updated train script.

--- scripts/train_dnn.py
+++ scripts/train_dnn.py
@@ -1,24 +1,22 @@
 #!python
 
 import argparse
+import numpy as np
 
-import tensorflow as tf
 from pfsspec.io.dataset import Dataset
-import pfsspec.ml.dnn.keras.models
-import pfsspec.ml.dnn.keras.train
+from pfsspec.ml.dnn.keras.densepyramid import DensePyramid
+from pfsspec.ml.dnn.keras.cnnpyramid import CnnPyramid
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--in", type=str, help="Training set file\n")
+    parser.add_argument("--out", type=str, help="Output directory\n")
     parser.add_argument('--param', type=str, help='Parameter to ml\n')
-    parser.add_argument('--gpu', type=str, help='GPUs to use\n')
-    parser.add_argument('--epochs', type=int, default=1, help='Number of epochs\n')
+    parser.add_argument('--gpus', type=str, help='GPUs to use\n')
+    parser.add_argument('--type', type=str, help='Type of network\n')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs\n')
+    parser.add_argument('--patience', type=int, default=5, help='Number of epochs to wait before early stop.\n')
     return parser.parse_args()
-
-def init_tensorflow():
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    session = tf.Session(config=config)
 
 def train_dnn(args):
     ts = Dataset()
@@ -29,14 +27,25 @@ def train_dnn(args):
     #model = pfsspec.ml.dnn.keras.models.create_dense_pyramid(ts.flux.shape[1], 1)
     #pfsspec.ml.dnn.keras.ml.train_dnn(ts.flux, ts.params[args.param], model, epochs=args.epochs)
 
-    nn_input = ts.flux.reshape((ts.flux.shape[0], ts.flux.shape[1], 1))
-    nn_labels = ts.params[args.param]
-    model = pfsspec.ml.dnn.keras.models.create_cnn_pyramid(nn_input.shape[1:], 1, levels=4)
-    pfsspec.ml.dnn.keras.train.train_dnn(nn_input, nn_labels, model, epochs=args.epochs)
+    input = ts.flux
+    labels = np.array(ts.params[args.param])
 
+    if args.type == 'dense':
+        model = DensePyramid()
+    elif args.type == 'cnn':
+        model = CnnPyramid()
+    else:
+        raise NotImplementedError()
+
+    model.gpus = args.gpus
+    model.patience = args.patience
+    model.epochs = args.epochs
+
+    model.train(input, labels)
+
+    # TODO: add various types of outputs
 
 def __main__(args):
-    init_tensorflow()
     train_dnn(args)
 
 __main__(parse_args())
