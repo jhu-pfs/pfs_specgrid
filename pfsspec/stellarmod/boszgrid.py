b586b4e347f4641b93ed9cfd34c651d9a3e20ae5
Laszlo Dobos <dobos@complex.elte.hu>
2019-09-27 12:28:13 -0400

    Handle full BOSZ grid.

--- pfsspec/stellarmod/boszgrid.py
+++ pfsspec/stellarmod/boszgrid.py
@@ -9,8 +9,10 @@ class BoszGrid(ModelGrid):
         super(BoszGrid, self).__init__(use_cont=True)
 
         self.params['Fe_H'] = ModelParam('Fe_H', np.arange(-2.5, 1.0, 0.25))
-        self.params['T_eff'] = ModelParam('T_eff', np.array([3500, 3750, 4000, 4250, 4500, 4750, 5000]))
-        self.params['log_g'] = ModelParam('log_g', np.arange(2.5, 5.5, 0.5))
+        self.params['T_eff'] = ModelParam('T_eff', np.hstack((np.arange(3500, 12500, 250),
+                                                              np.arange(12500, 20000, 500),
+                                                              np.arange(20000, 36000, 1000))))
+        self.params['log_g'] = ModelParam('log_g', np.arange(0, 5.5, 0.5))
         self.params['C_M'] = ModelParam('C_M', np.arange(-0.75, 0.75, 0.25))
         self.params['a_Fe'] = ModelParam('a_Fe', np.arange(-0.25, 0.75, 0.25))
 
