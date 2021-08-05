301e5e7d9938d2622f82693da9913f190a22c838
Laszlo Dobos <dobos@complex.elte.hu>
2021-08-05 12:46:10 -0400

    Added new timer utility class.

---
+++ pfsspec/util/timer.py
@@ -0,0 +1,31 @@
+import time
+import logging
+
+class Timer:
+    def __init__(self, message, print_to_console=False, print_to_log=True):
+        self.message = message
+        self.print_to_console = print_to_console
+        self.print_to_log = print_to_log
+        self.logger = logging.getLogger() if self.print_to_log else None
+
+        self.start = time.perf_counter()
+
+    def __enter__(self):
+        self.banner()
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.stamp()
+
+    def banner(self):
+        if self.print_to_console:
+            print(self.message)
+        if self.logger:
+            self.logger.info(self.message)
+
+    def stamp(self):
+        elapsed = time.perf_counter() - self.start
+        msg = '... done in {} sec.'.format(elapsed)
+        if self.print_to_console:
+            print(msg)
+        if self.logger:
+            self.logger.info(msg)
